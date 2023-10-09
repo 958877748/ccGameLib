325d1d95524e718a9154f52ae81eb552e3c8091c
郭大侠的剑 <31437616+958877748@users.noreply.github.com>
2023-10-09 21:35:12 +0800

    Update and rename slider.ts to SliderBar.ts

--- SliderBar.ts
+++ SliderBar.ts
@@ -1,20 +1,13 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 import { gamedata } from "./IGameData"
-import switchText from "./switchText"
+import SwitchText from "./SwitchText"
 
 const { ccclass, property } = cc._decorator
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class SliderBar extends cc.Component {
 
-    @property(switchText)
-    text: switchText = null
+    @property(SwitchText)
+    text: SwitchText = null
 
     @property
     offx = 0
